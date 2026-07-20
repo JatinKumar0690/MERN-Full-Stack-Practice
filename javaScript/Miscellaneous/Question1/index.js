4593742ee1498fb661761512e0bcc27cbfc2e3cc
Jatin Kumar <anshkhoba@gmail.com>
2026-07-20 15:00:15 +0530

    solved first question

---
+++ javaScript/Miscellaneous/Question1/index.js
@@ -0,0 +1,7 @@
+let userInput = prompt("Enter a number:");
+
+if (userInput % 5 == 0) {
+  console.log(userInput, "is a multiple of 5");
+} else {
+  console.log(userInput, "isn't a multiple of 5");
+}
