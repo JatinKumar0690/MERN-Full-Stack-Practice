dbda0fe78a16d099d75573e1045d29c05196b193
Jatin Kumar <anshkhoba@gmail.com>
2026-07-21 16:39:05 +0530

    solved fourth question

---
+++ javaScript/Miscellaneous/Question4/index.js
@@ -0,0 +1,9 @@
+let gameNum = 100;
+let userNum = prompt("Enter the correct number");
+
+while (gameNum != userNum) {
+  //game
+  userNum = prompt("Enter the number again");
+}
+
+console.log("Congratulations you won the game");
